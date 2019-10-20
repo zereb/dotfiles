86ddc97b4ad40da9eac99da0309e04414b567f7a
oleg <zizereb@gmail.com>
2019-10-20 22:13:20 +0500

    pswd wpdo i3 float window fix

--- fish/config.fish
+++ fish/config.fish
@@ -6,6 +6,7 @@ set -xU TERMINAL "alacritty"
 set -xU FILE "ranger"
 set -xU BROWSER "qutebrowser"
 set -xU _JAVA_OPTIONS "-Dawt.useSystemAAFontSettings=lcd"
+set -xU PATH_TO_FX "~/Documents/.SDK/javafx-sdk-12.0.1/"
 set -xU ANDROID_HOME "~/Android/Sdk"
 if status --is-login
     if test -z "$DISPLAY" -a $XDG_VTNR -eq 1
