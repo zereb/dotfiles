d70625e7d90a161cef08d6e5e233a1efd8d81712
Oleg Plett <o.plett@vintersaga.ru>
2022-10-08 18:17:01 +0500

    emacs descriptions

--- emacs/modules/completion-setup.el
+++ emacs/modules/completion-setup.el
@@ -1,12 +1,17 @@
 ;;; in minibuffer completion
+;https://github.com/oantolin/orderless
+;This package provides an orderless completion style that divides the pattern into space-separated components, and matches candidates that match all of the components in any order. Each component can match in any one of several ways: literally, as a regexp, as an initialism, in the flex style, or as multiple word prefixes. By default, regexp and literal matches are enabled.
 (use-package orderless
   :ensure t
   :custom
   (completion-styles '(orderless basic))
   (completion-category-overrides '((file (styles basic partial-completion)))))
 
+;many consult commands
 (use-package consult)
 
+;https://github.com/minad/vertico
+;minibuffer completion [bottom one]
 (use-package vertico
   :init
   (vertico-mode)
@@ -19,16 +24,19 @@
 	     ("C-k" . vertico-previous)
 	     ("ESC" . vertico-exit)))
 
+;save minibuffer history
 (use-package savehist
   :init
   (savehist-mode))
 
+;better minibufer description
 (use-package marginalia
   :after vertico
   :custom
   (marginalia-annotators '(marginalia-annotators-heavy marginalia-annotators-light nil))
   :init
   (marginalia-mode))
+
 ;;; in buffer completion
 (use-package company
   :config
